99d4df2a094115e264faf526d30755ffa98824ff
cliuff <35420834+cliuff@users.noreply.github.com>
2020-04-27 02:57:29 +0800

    Code optimization

--- cool_app/src/main/java/com/madness/collision/unit/cool_app/MyBridge.kt
+++ cool_app/src/main/java/com/madness/collision/unit/cool_app/MyBridge.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Clifford Liu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.madness.collision.unit.cool_app
 
 import android.appwidget.AppWidgetManager
@@ -16,6 +32,7 @@ object MyBridge: Bridge() {
         return MyUnit()
     }
 
+    @Suppress("unused")
     fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
         AppInfoWidgetFun.updateAppWidget(context, appWidgetManager, appWidgetId)
     }
