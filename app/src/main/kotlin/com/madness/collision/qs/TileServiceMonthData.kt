4512bb8678fc0407a92cf923c4c73916e8a888f0
cliuff <35420834+cliuff@users.noreply.github.com>
2020-09-06 14:00:07 +0800

    Month Data Usage: use coroutines, use tile subtitle on Android 10

--- app/src/main/kotlin/com/madness/collision/qs/TileServiceMonthData.kt
+++ app/src/main/kotlin/com/madness/collision/qs/TileServiceMonthData.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Clifford Liu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.madness.collision.qs
 
 import android.annotation.TargetApi
@@ -6,13 +22,14 @@ import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.os.Build
-import android.os.Handler
 import android.provider.Settings
 import android.service.quicksettings.TileService
 import android.widget.Toast
+import com.madness.collision.BuildConfig
 import com.madness.collision.R
 import com.madness.collision.util.X
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.util.*
 
@@ -20,13 +37,26 @@ import java.util.*
 internal class TileServiceMonthData : TileService() {
     override fun onTileAdded() {
         TileCommon.inactivate(qsTile)
-        Handler().postDelayed({
+        GlobalScope.launch {
+            delay(10_000)
             ensurePermission()
-        }, 10000)
+        }
     }
+
+    private var tileLabel: String = ""
+        get() {
+            if (field.isEmpty()) {
+                field = getString(R.string.tileData)
+            }
+            return field
+        }
 
     override fun onStartListening() {
         TileCommon.inactivate(qsTile)
+        // todo remove fix
+        if (BuildConfig.VERSION_CODE > 20080801 && X.aboveOn(X.Q)) {
+            qsTile.label = tileLabel
+        }
         ensurePermission()
         update()
     }
@@ -44,18 +74,23 @@ internal class TileServiceMonthData : TileService() {
             action = Settings.ACTION_USAGE_ACCESS_SETTINGS
             flags = Intent.FLAG_ACTIVITY_NEW_TASK
         })
-        Handler().postDelayed({
+        GlobalScope.launch {
+            delay(1000)
             X.toast(applicationContext, R.string.tileDataUsageAccess, Toast.LENGTH_LONG)
-        }, 1000)
+        }
     }
 
     private fun update(){
         val qsTile = qsTile ?: return
-        val previous = qsTile.label
-        qsTile.label = "..."
+        val previous = if (X.aboveOn(X.Q)) qsTile.subtitle else qsTile.label
+        if (X.aboveOn(X.Q)) {
+            qsTile.subtitle = "..."
+        } else {
+            qsTile.label = "..."
+        }
         qsTile.updateTile()
-        var totalGbDay: Double = 0.toDouble()
-        var totalGbMonth: Double = 0.toDouble()
+        var totalGbDay = 0E0
+        var totalGbMonth = 0E0
         GlobalScope.launch {
             val cal = Calendar.getInstance()
             cal.set(Calendar.HOUR_OF_DAY, 0)
@@ -67,22 +102,31 @@ internal class TileServiceMonthData : TileService() {
             val timeFirstDayOfMonth = cal.timeInMillis
             val manager = getSystemService(Context.NETWORK_STATS_SERVICE) as NetworkStatsManager? ?: return@launch
             try {
-                val gb: Double = 1073741824.toDouble() // 2^30
+                val gb = 1073741824E0 // 2^30
+                val time = System.currentTimeMillis()
                 // current day traffic mobile data usage
-                val usageDay = manager.querySummaryForDevice(ConnectivityManager.TYPE_MOBILE, null, timeDay, System.currentTimeMillis())
+                val usageDay = manager.querySummaryForDevice(
+                        ConnectivityManager.TYPE_MOBILE, null, timeDay, time)
                 val receivedDay = usageDay.rxBytes
                 val transmittedDay = usageDay.txBytes
                 val totalDay = receivedDay + transmittedDay
                 totalGbDay = totalDay / gb
                 // month traffic mobile data usage
-                val usageMonth = manager.querySummaryForDevice(ConnectivityManager.TYPE_MOBILE, null, timeFirstDayOfMonth, System.currentTimeMillis())
+                val usageMonth = manager.querySummaryForDevice(
+                        ConnectivityManager.TYPE_MOBILE, null, timeFirstDayOfMonth, time)
                 val receivedMonth = usageMonth.rxBytes
                 val transmittedMonth = usageMonth.txBytes
                 val totalMonth = receivedMonth + transmittedMonth
                 totalGbMonth = totalMonth / gb
-            }catch (e: Exception){ e.printStackTrace() }
+            } catch (e: Exception){ e.printStackTrace() }
         }.invokeOnCompletion {
-            qsTile.label = if (totalGbDay == 0.toDouble() || totalGbMonth == 0.toDouble()) previous else String.format("%.2f•%.2fGB", totalGbDay, totalGbMonth)
+            val newVal = if (totalGbDay == 0E0 || totalGbMonth == 0E0) previous
+            else String.format("%.2f • %.2f GB", totalGbDay, totalGbMonth)
+            if (X.aboveOn(X.Q)) {
+                qsTile.subtitle = newVal
+            } else {
+                qsTile.label = newVal
+            }
             qsTile.updateTile()
         }
     }
