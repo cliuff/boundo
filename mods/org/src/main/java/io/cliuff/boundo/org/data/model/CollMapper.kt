c9cf29f6fdcce6cbaa596dfeb8657bf7ed9093f3
cliuff <ballupon@gmail.com>
2024-10-30 19:46:39 +0900

    New org models

---
+++ mods/org/src/main/java/io/cliuff/boundo/org/data/model/CollMapper.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2024 Clifford Liu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.cliuff.boundo.org.data.model
+
+import io.cliuff.boundo.org.db.model.OrgCollEntity
+import io.cliuff.boundo.org.model.CollInfo
+import io.cliuff.boundo.org.model.CompColl
+
+fun CollInfo.toEntity() =
+    OrgCollEntity(
+        id = id,
+        name = name,
+    )
+
+fun OrgCollEntity.toModel(groupCount: Int) =
+    CollInfo(
+        id = id,
+        name = name,
+        groupCount = groupCount,
+    )
+
+
+fun CompColl.toEntity() =
+    OrgCollEntity(
+        id = id,
+        name = name,
+    )
+
+
+fun List<CollInfo>.toEntities() = map(CollInfo::toEntity)
